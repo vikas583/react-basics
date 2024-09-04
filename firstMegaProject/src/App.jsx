1f807f2b48998389fa3f0101a4d39f178082f7cb
vikas.arora <vikas.arora@tiebreaker-ai.com>
2024-09-04 08:27:58 +0530

    components and tailwind

--- firstMegaProject/src/App.jsx
+++ firstMegaProject/src/App.jsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from "react-redux"
 import './App.css'
+import authService from "./appwrite/auth.service"
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from "./components/index"
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
 
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog App with App Write</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      }
+      else {
+        dispatch(logout())
+      }
+
+    })
+      .finally(() => { setLoading(false) })
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        Todo: <Outlet />
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
